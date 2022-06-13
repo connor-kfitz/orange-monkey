e77df2547f503a252e1f90226cc5365845aff7fa
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-13 19:05:56 -0400

    Updated code with class functions

--- index.js
+++ index.js
@@ -6,6 +6,77 @@ var managerQuestions = ['What is the manager\'s name?', 'What is the manager\'s 
 var engineerQuestions = ['What is the engineer\'s name?', 'What is the engineer\'s employee ID?', 'What is the engineer\'s email address?', 'What is the engineer\'s Github link?'];
 var internQuestions = ['What is the intern\'s name?', 'What is the intern\'s employee ID?', 'What is the intern\'s email address?', 'What is the intern\'s school?'];
 
+var employeeName = '';
+var employeeRole = '';
+var employeeID = '';
+var employeeEmail = '';
+var employeeInfo = '';
+
+var employeeSelection = '';
+var newEmployee;
+
+// Classes
+class Employee {
+  constructor(employeeName, employeeID, employeeEmail){
+    this.employeeName = employeeName;
+    this.employeeID = employeeID;
+    this.employeeEmail = employeeEmail;
+  }
+
+  getName(){
+    return this.employeeName;
+  }
+  getID(){
+    return this.employeeID;
+  }
+  getEmail(){
+    return this.employeeEmail;
+  }
+  getRole(){
+    return 'Employee';
+  }
+}
+
+// Sub-Classes
+class Manager extends Employee {
+  constructor(employeeName, employeeID, employeeEmail, officeNumber){
+    super(employeeName, employeeID, employeeEmail);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber(){
+    return this.officeNumber;
+  }
+  getRole(){
+    return 'Manager';
+  }
+}
+
+class Engineer extends Employee {
+  constructor(employeeName, employeeID, employeeEmail, github){
+    super(employeeName, employeeID, employeeEmail);
+    this.github = github;
+  }
+  getGithub(){
+    return this.github
+  }
+  getRole(){
+    return 'Engineer';
+  }
+}
+
+class Intern extends Employee {
+  constructor(employeeName, employeeID, employeeEmail, school){
+    super(employeeName, employeeID, employeeEmail);
+    this.school = school;
+  }
+  getSchool(){
+    return this.school
+  }
+  getRole(){
+    return 'Intern';
+  }
+}
+
 // HTML Skeleton
 var htmlSkeletonStart = `
 <!DOCTYPE html>
@@ -31,29 +102,51 @@ var htmlSkeletonEnd = `
 
 </html>
 `
-
-// var htmlEmployeeCard = `
-// <div class="flex">
-//         <div class = 'employeeCard flex'>
-//             <div class = 'cardTop flex'>
-//                 <h1>${employeeName}</h1>
-//                 <h2>${employeeRole}</h2>
-//             </div>
-//             <div class = 'cardBottom flex'>
-//                 <p>ID: ${employeeID}</p>
-//                 <p>E-mail: ${employeeEmail}</p>
-//                 <p>${employeeInfo}</p>
-//             </div>
-//         </div>
-//     </div>
-// `
-
 // Functions
 function createHTML(data) {
     fs.writeFile(filename, data, (err) =>
       err ? console.log(err) : console.log('Congratulations, your HTML file was successfully created')
     );
 }
+
+function appendEmployeeCard() {
+  var htmlEmployeeCard = `
+<div class="flex">
+        <div class = 'employeeCard flex'>
+            <div class = 'cardTop flex'>
+                <h1>${newEmployee.getName()}</h1>
+                <h2>${newEmployee.getRole()}</h2>
+            </div>
+            <div class = 'cardBottom flex'>
+                <p>ID: ${newEmployee.getID()}</p>
+                <p>E-mail: ${newEmployee.getEmail()}</p>`
++ appendEmployeeSpecific
+`
+            </div>
+        </div>
+    </div>`;
+
+htmlSkeletonStart += htmlEmployeeCard;
+}
+
+function appendEmployeeSpecific(){
+  if(newEmployee.getRole() == 'Manager'){
+    return `
+                <p>Office Number: ${newEmployee.getOfficeNumber()}</p>  
+`
+  }
+  else if(newEmployee.getRole() == 'Engineer'){
+    return `
+                <p>Office Number: ${newEmployee.getGithub()}</p>  
+`
+  }
+  else if(newEmployee.getRole() == 'Intern'){
+    return `
+    <p>Office Number: ${newEmployee.getSchool()}</p>  
+`
+  }
+}
+
 
 function managerFunction(){
  inquirer
@@ -79,6 +172,11 @@ function managerFunction(){
       message: managerQuestions[3],
     }
  ])
+ .then((data) => {
+  newEmployee = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOffice);
+  appendEmployeeCard();
+ })
+ 
 }
 
 function engineerFunction(){
@@ -86,25 +184,29 @@ function engineerFunction(){
   .prompt([
     {
         type: 'input',
-        name: 'managerName',
+        name: 'engineerName',
         message: engineerQuestions[0],
     },
     {
       type: 'input',
-      name: 'managerID',
+      name: 'engineerID',
       message: engineerQuestions[1],
     },
     {
       type: 'input',
-      name: 'managerEmail',
+      name: 'engineerEmail',
       message: engineerQuestions[2],
     },
     {
       type: 'input',
-      name: 'managerOffice',
+      name: 'engineerGithub',
       message: engineerQuestions[3],
     }
  ])
+ .then((data) => {
+  newEmployee = new Engineer(data.engineerName, data.engineerID, data.engineerEmail, data.engineerGithub)
+  appendEmployeeCard();
+ })
 }
 
 function internFunction(){
@@ -112,28 +214,94 @@ function internFunction(){
   .prompt([
     {
         type: 'input',
-        name: 'managerName',
+        name: 'internName',
         message: internQuestions[0],
     },
     {
       type: 'input',
-      name: 'managerID',
+      name: 'internID',
       message: internQuestions[1],
     },
     {
       type: 'input',
-      name: 'managerEmail',
+      name: 'internEmail',
       message: internQuestions[2],
     },
     {
       type: 'input',
-      name: 'managerOffice',
+      name: 'internOffice',
       message: internQuestions[3],
     }
  ])
+ .then((data) => {
+  employeeName = data.internName;
+  employeeRole = 'Intern';
+  employeeID = data.internID;
+  employeeEmail = data.internEmail;
+  employeeInfo = data.internOffice;
+  appendEmployeeCard();
+  console.log(htmlSkeletonStart);
+ })
 }
 
+function addEmployee(){
+  inquirer
+  .prompt([
+    {
+        type: 'list',
+        name: 'addEmployeePrompt',
+        message: 'Would you like to add an Engineer, Intern, or finish?',
+        choices: ['Engineer', 'Intern', 'Finish']
+    },
+  ])
+  .then((data) => {
+    employeeSelection = data.addEmployeePrompt;
+    if(employeeSelection == 'Engineer'){
 
+    }
+    else if (employeeSelection == 'Intern'){
+
+    }
+    else {
+
+    }
+  })
+}
+
+function addAnother(){
+  inquirer
+  .prompt([
+    {
+        type: 'list',
+        name: 'continue',
+        message: 'Would you like to add another',
+    }
+  ])
+}
+
+// function add
+
+// function init(){
+//   managerFunction();
+//   var employeeType = '';
+//   var i = 1;
+//   addEmployee();
+//   if (employeeSelection = 'Finish'){
+
+//   }
+//   while(i = 1) {
+//     if(employeeType == 'engineer'){
+//       engineerFunction();
+//     }
+//     else if(employeeType == 'intern'){
+//       internFunction();
+//     }
+  
+//   }
+// }
+
+
+managerFunction();
 // engineerFunction();
 // internFunction();
 // init();
